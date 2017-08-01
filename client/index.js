320475aa7b796d88a4844a9c0ddfc83315e3eceb
willin <willin@willin.org>
2017-08-01 16:49:15 +0800

    更换mqtt broker框架

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 const mqtt = require('mqtt');
 
-const client = mqtt.connect('mqtt://127.0.0.1', {
+const client = mqtt.connect('mqtts://127.0.0.1', {
+  // rejectUnauthorized: false,
   username: 'willin',
   password: 'test'
 });
@@ -11,3 +12,8 @@ client.on('message', (topic, message) => {
   // message is Buffer
   console.log(message.toString());
 });
+
+client.on('close', () => {
+  client.end();
+});
+
--- server/index.js
+++ server/index.js
@@ -1,44 +1,23 @@
 const fs = require('fs');
+const aedes = require('aedes')();
 const path = require('path');
-const mqttServer = require('./lib/mqtt-server');
 
-const servers = mqttServer({
-  mqtt: 'tcp://localhost:1883',
-  mqtts: 'ssl://localhost:8883',
-  mqttws: 'ws://localhost:1884',
-  mqtwss: 'wss://localhost:8884'
-}, {
-  ssl: {
-    key: fs.readFileSync(path.join(__dirname, '../extra/localhost.key')),
-    cert: fs.readFileSync(path.join(__dirname, '../extra/localhost.crt'))
-  },
-  emitEvents: true // default
-}, (client) => {
-  client.connack({
-    returnCode: 0
-  });
-  client.on('connect', function (data) {
-    const username = data.username;
-    const password = data.password.toString();
-    console.log(username, password);
-    // 发布测试
-    client.publish({
-      topic: 'deviceId',
-      payload: 'hello world'
-    });
-    if (username !== 'willin' || password !== 'willin') {
-      // 中断连接
-      this.disconnect();
-    }
-  });
-  // client.on('data', (data) => {
-  //   if (data.cmd === 'connect') {
-  //     console.log(data.username, data.password.toString());
-  //   }
-  //   console.log(data);
-  // });
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '../extra/localhost.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../extra/localhost.crt'))
+};
+
+const server = require('tls').createServer(options, aedes.handle);
+
+server.listen(8883, () => {
+  console.log('server started and listening on port 8883');
 });
 
-servers.listen(() => {
-  console.log('listening!');
+aedes.on('client', (client) => {
+  console.log('new client', client.id);
+  client.publish({
+    topic: 'deviceId',
+    payload: 'hello world'
+  });
+  client.close();
 });
